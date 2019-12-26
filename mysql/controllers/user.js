1bceb342ef0ed5cca945be4ef1b516925a7e38c9
zhangye <zhangye@mastercom.cn>
2019-12-26 15:56:18 +0800

    add mysql

---
+++ mysql/controllers/user.js
@@ -0,0 +1,25 @@
+class User {
+
+    constructor() {
+        this.db = require('../dao/connect');
+    }
+
+    register(json) {
+        return new Promise((resolve, reject) => {
+            this.db.select(['name'], 'user', { name: json.name }).then(result => {
+                if (result.length === 0) {
+                    this.db.insert(json, 'user').then(res => {
+                        resolve({code: 0, msg: 'ok'});
+                    }).catch(err => {
+                        reject({code: -1, msg: 'error'});
+                    });
+                } else {
+                    reject({code: -1, msg: '已经存在此账号'});
+                }
+            });
+        });
+    }
+
+}
+
+module.exports = new User();
---
+++ mysql/test.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    next();
+});
+
+
+app.get('/user/register', (req, res) => {
+    let user = require('./controllers/user');
+    // let info = {
+    //     name: req.body.username,
+    //     password: req.body.password
+    // };
+    let info = {
+        username: "123ur",
+        password: "123ps"
+    };
+    user.register(info).then(result => {
+        res.json(result);
+    }).catch(err => {
+        res.json(err);
+    });
+});
+
+app.listen(5100);
+console.log("strat 5100");
