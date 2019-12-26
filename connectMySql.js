8d24e6a0836c1b9167f290b5a58c27dfb3c0e3d1
zhangye <zhangye@mastercom.cn>
2019-12-26 10:35:25 +0800

    select * from tb_user

--- connectMySql.js
+++ connectMySql.js
@@ -12,3 +12,19 @@ connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
 });
+
+
+let sql = 'SELECT * FROM tb_user';
+//查
+connection.query(sql, function (err, result) {
+    if (err) {
+        console.log('[SELECT ERROR] - ', err.message);
+        return;
+    }
+
+    console.log('--------------------------SELECT----------------------------');
+    console.log(result);
+    console.log('------------------------------------------------------------\n\n');
+});
+
+connection.end();
